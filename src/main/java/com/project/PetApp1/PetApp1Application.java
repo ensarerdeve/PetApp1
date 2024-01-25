22af0297bde302c321acf220b9e55728c0965fda
Ensar <erdeveensar9@gmail.com>
2024-01-25 14:52:34 +0300

    Initial commit

---
+++ src/main/java/com/project/PetApp1/PetApp1Application.java
@@ -0,0 +1,13 @@
+package com.project.PetApp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/PetApp1/PetApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.PetApp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
