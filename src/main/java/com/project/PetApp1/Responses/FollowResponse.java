7e2c7b6477e0b7ce521c71639d01b29617936b6b
OpsYY <eget06@gmail.com>
2024-03-06 13:00:56 +0300

    follow update
    followed/followeruserid - date

--- src/main/java/com/project/PetApp1/Responses/FollowResponse.java
+++ src/main/java/com/project/PetApp1/Responses/FollowResponse.java
@@ -2,11 +2,22 @@ package com.project.PetApp1.Responses;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class FollowResponse {
     private Long id;
     private String followerUserName;
+    private Long followerUserId;
     private String followedUserName;
+    private Long followedUserId;
+    private Date createDate;
+    private String formattedDate;
+
+
+
+
+
 
 
 }
