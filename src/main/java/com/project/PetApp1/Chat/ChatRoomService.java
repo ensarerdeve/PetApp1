7220d687f114a419510d6fced8e5089b22358ac4
OpsYY <eget06@gmail.com>
2024-04-28 21:54:41 +0300

    Update ChatRoomService.java

--- src/main/java/com/project/PetApp1/Chat/ChatRoomService.java
+++ src/main/java/com/project/PetApp1/Chat/ChatRoomService.java
@@ -38,6 +38,6 @@ public class ChatRoomService {
                 .build();
         chatRoomRepository.save(senderRecipient);
         chatRoomRepository.save(recipientSender);
-        return null;
+        return chatId;
     }
 }
